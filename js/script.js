bc8e6ead0689baf745eb428a32b98c5000fba82b
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-28 16:04:46 +0200

    Aggiunto esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// Inserisci il codice qui
+var app = new Vue(
+  {
+    el: '#listaToDo',
+    data: {
+      newItem: "",
+      lista: [
+        'fare il bucato',
+        'guardare netflix',
+        'fare il pranzo',
+        'finire il compito',
+        'andare a dormire'
+      ]
+    },
+    methods: {
+      removeItem: function(itemIndex) {
+        this.lista.splice(itemIndex, 1);
+      },
+      addItem: function() {
+        this.lista.push(this.newItem);
+      }
+    }
+  }
+);
